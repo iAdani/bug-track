3347c0770bfbb641e2af036089089987d330a2ef
iAdani <guy3102@gmail.com>
2025-05-16 14:24:36 +0300

    Text change

--- Backend/server.js
+++ Backend/server.js
@@ -9,5 +9,5 @@ const PORT = process.env.PORT || 5000;
 app.use('/api/test', require('./routes/testRoutes'));
 
 connectDB().then(() => {
-  app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+  app.listen(PORT, () => console.log(`** Server running on port ${PORT} **`));
 });
